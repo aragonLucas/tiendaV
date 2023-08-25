21ea018eb6a419643b22a9eab3d6ad99d0cf384f
LucasAragon <aragonlucas1989@gmail.com>
2023-08-25 01:43:22 -0300

    se realizan cambios en los menus inclusiva para responsiv

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+/*-------menu de la right---------*/
+
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+/*------menu de la izq--------*/
+const menuHamIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
+
+/*-------evento de click--------*/
 menuEmail.addEventListener("click", toggleDesktopMenu);
+
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+
+
+
+/* func para que aparezca el menu de la right*/
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
+}
+
+
+/* func para que aparezca el menu de la izq*/
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
