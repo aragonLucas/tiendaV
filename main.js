dc0109cee0913c3223f40f9a4aa9651aeeb4a27f
LucasAragon <aragonlucas1989@gmail.com>
2023-08-28 14:26:07 -0300

    continuamos con la creacion de los menus en js

--- main.js
+++ main.js
@@ -7,10 +7,17 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 
+/*menu carrito*/
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
+
 /*-------evento de click--------*/
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+            /*menuCarrito*/
+menuCarritoIcon.addEventListener ("click" , toggleCarritoAside);
 
 
 
@@ -18,11 +25,36 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 /* func para que aparezca el menu de la right*/
 
 function toggleDesktopMenu() {
+
+    const isAsideClosed = aside.classList.contains("inactive")
+    if(!isAsideClosed) {
+        aside.classList.add("inactive")
+    }
     desktopMenu.classList.toggle("inactive")
 }
 
 
 /* func para que aparezca el menu de la izq*/
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.toggle("inactive")
+    if(!isAsideClosed){
+        aside.classList.add("inactive")
+
+    }
     mobileMenu.classList.toggle("inactive")
+
 }
+
+/*func para el menu carrito */
+function toggleCarritoAside() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+    const isAsideClosed = aside.classList.contains("inactive")
+
+    if(!isMobileMenuClosed){
+    mobileMenu.classList.add("inactive")
+   }
+
+   aside.classList.toggle("inactive")
+}
+    
